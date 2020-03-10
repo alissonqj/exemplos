71bafe268bbd3bd9e743a328316121b495805621
alissonqj <alissonqj@hotmail.com>
2020-03-10 18:22:15 -0300

    Alteraçoes no domain.py

--- copa_transparente/domain_old.py
+++ copa_transparente/domain_old.py
@@ -1,53 +1,51 @@
-#domain.py
-from decimal import Decimal
+import decimal
 
 class Column:
     def __init__(self, name, kind, description=""):
         self._name = name
         self._kind = kind
         self._description = description
+        self._is_pk = False
 
     def __str__(self):
-        _str = "Col: {} : {} {}".format(
-                self._name,
-                self._kind,
-                self._description
-                )
+        _str = "Col: {} : {} {}".format(self._name,
+                                        self._kind,
+                                        self._description)
+        if self._is_pk:
+            _str = "({}) {}".format("PK", _str)
         return _str
 
     @staticmethod
     def validate(kind, data):
-        if kind == "bigint":
+        if kind == 'bigint':
             if isinstance(data, int):
                 return True
             return False
-        elif kind == "varchar":
+        elif kind == 'varchar':
             if isinstance(data, str):
                 return True
             return False
-        elif kind == "numeric":
+        elif kind == 'numeric':
             try:
-                _ = Decimal(data)
-            except Exception:
+                val = decimal.Decimal(data)
+            except:
                 return False
             return True
 
+
 class PrimaryKey(Column):
-    def __init__(self, name, kind, description=None):
+    def __init__(self, table, name, kind, description=None):
+        super().__init__(name, kind, description=description)
         self._is_pk = True
-        super().__init__(name, kind, description)
-    
-    def __str__(self):
-        _str = super().__str__()
-        return "{} - {}".format("PK", _str)
+
 
 class Relationship:
     def __init__(self, name, _from, to, on):
-
         self._name = name
         self._from = _from
         self._to = to
         self._on = on
+
 
 class DataTable:
     def __init__(self, name):
@@ -57,10 +55,28 @@ class DataTable:
         self._referenced = []
         self._data = []
 
-    def add_column(self, name, kind, description= ""):
+    def _get_name(self):
+        return self._name
+
+    def _set_name(self, _name):
+        self._name = _name
+
+    def _del_name(self):
+        raise AttributeError("Não pode deletar esse atributo")
+
+    name = property(_get_name, _set_name, _del_name)
+    references = property(lambda self: self._references)
+    referenced = property(lambda self: self._referenced)
+
+    def add_column(self, name, kind, description=""):
+        self._validate_kind(kind)
         column = Column(name, kind, description=description)
         self._columns.append(column)
         return column
+
+    def _validate_kind(self, kind):
+        if not kind in ('bigint', 'numeric', 'varchar'):
+            raise Exception("Tipo inválido")
 
     def add_references(self, name, to, on):
         relationship = Relationship(name, self, to, on)
